a9cfdc5769606cd640379cda812c691a27783f76
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2022-02-03 20:46:01 -0500

    added files

---
+++ sketch.js
@@ -0,0 +1,68 @@
+
+
+	var myVoice = new p5.Speech(); // new P5.Speech object
+
+	var listbutton; // button
+
+	function setup()
+	{
+		// graphics stuff:
+		createCanvas(windowWidth , windowHeight) ;
+
+
+    input = createInput();
+    input.position(20, 80);
+    //input.size(10000000);
+
+    button = createButton( "klick me" );
+    button.position(200, 80);
+    button.mouseClicked(talk);
+    myVoice.setRate(0.9);
+
+	}
+
+	function draw()
+	{
+    
+		
+	}
+
+  function talk(){
+    
+    x = input.value();
+    const myArray = x.split(" ");
+
+    for(let i = 2; i < myArray.length; i+=3){
+      myArray[i] = "rats";
+    }
+
+    str = "";
+    for(let j = 0; j < myArray.length; j++){
+      str += myArray[j] + " ";
+    }
+    
+    myVoice.speak(str);
+    
+  }
+
+
+
+
+  function keyPressed() {
+
+    
+    x = input.value();
+    const myArray = x.split(" ");
+
+    for(let i = 2; i < myArray.length; i+=3){
+      myArray[i] = "rats";
+    }
+
+    str = "";
+    for(let j = 0; j < myArray.length; j++){
+      str += myArray[j] + " ";
+    }
+    if(key == 'S'){
+      myVoice.speak(str);
+    }
+}
